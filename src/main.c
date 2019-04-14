f53511abaf81c2a1b15e01bc89f20540a046a28b
serge <serge_cornet@hotmail.com>
2019-04-14 16:55:42 +0200

    change in main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <pthread.h>
+#include <semaphore.h>
 #include "include/buffer.h"
 #include "include/main.h"
 
 int main(int argc, char const *argv[]) {
+  /*d'abord recupérer les arguments dans la structure*/
+  /*créer ldeux sémpahores et un mutex (empty, full, mutex buf-hash)*/
+  /*faire appel aux threads producteur/consommateur*/
+  int sem_init(&empty, 0, 0);
+  int sem_init(&full, 0, nthreads);
   return 0; /*declare l'argument args_t afin d'avoir les arguments en ordre */
 
 };
