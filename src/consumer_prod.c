ec2a1ce1d4efb8a7c0c2de6044d578a1100031a4
serge <serge_cornet@hotmail.com>
2019-05-09 19:37:07 +0200

    Dernière modification qui fonctionne

--- src/consumer_prod.c
+++ src/consumer_prod.c
@@ -9,41 +9,31 @@ void* consumer_routine(void * tru_cons_prod){
       sem_wait(&(tab_circulaire->full));
       pthread_mutex_lock(&(tab_circulaire->mutex));
 
-      if(tab_circulaire->i<tab_circulaire->count){ //tant qu'il y a plus qu'un élement dans le premier buffer..
+      if(tab_circulaire->i<tab_circulaire->count){
+        printf("%d, %d coke est un con \n",tab_circulaire->i,tab_circulaire->count); //tant qu'il y a plus qu'un élement dans le premier buffer..
         char* res = malloc(sizeof(char)*16+1);
         uint8_t* tab32 = malloc(sizeof(uint8_t)*32);//compte le nombre de string ajouté dans le deuxième buffer.
         tab_circulaire->i = tab_circulaire->i+1;
         tab_circulaire1->count1 = tab_circulaire1->count1+1;
         deleteByte_buff(tab32);
         //printf("unlock\n");
-
-        if(tab_circulaire->boucle_cons==0){
-          pthread_mutex_unlock(&(tab_circulaire->mutex));
-          sem_post(&(tab_circulaire->full));
-        }
-        else{
-          pthread_mutex_unlock(&(tab_circulaire->mutex));
-          sem_post(&(tab_circulaire->empty));
-        }
+      pthread_mutex_unlock(&(tab_circulaire->mutex));
         //printf("rever\n");
-          if(reversehash(tab32, res, 16)){
-            printf("%s\n", res);
-            sem_wait(&(tab_circulaire->full));
-            //printf("lock\n");
-            sem_wait(&(tab_circulaire1->empty1));
+      if(reversehash(tab32, res, 16)){
+
+        sem_wait(&(tab_circulaire1->empty1));
             //printf("empty\n");
-            pthread_mutex_lock(&(tab_circulaire1->mutex1));
+        pthread_mutex_lock(&(tab_circulaire1->mutex1));
             //printf("222\n");
-            ajoutString_Buff(res);
+        ajoutString_Buff(res);
+        pthread_mutex_lock(&(tab_circulaire->mutex));
+        pthread_mutex_unlock(&(tab_circulaire1->mutex1));
 
-            pthread_mutex_unlock(&(tab_circulaire1->mutex1));
-
-            sem_post(&(tab_circulaire1->full1));
-            printf("string(res)\n");
-          }
-
+        sem_post(&(tab_circulaire1->full1));
         free(res);
         free(tab32);
+          }
+
         if(tab_circulaire->boucle_cons==0 ){ // si le producteur a fini de mettre tous les hash dans le premier buffer.
           pthread_mutex_unlock(&(tab_circulaire->mutex));
           sem_post(&(tab_circulaire->full));
